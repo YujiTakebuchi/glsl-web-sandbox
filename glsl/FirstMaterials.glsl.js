d9fa345173a4f5a226e7fc9d10c6fa9db4c23d55
YujiTakebuchi <takeyou.swift@gmail.com>
2024-01-05 02:30:43 +0900

    色変化のsin波を正規化、黒の時間が長くなっていたのを修正

--- glsl/FirstMaterials.glsl.js
+++ glsl/FirstMaterials.glsl.js
@@ -10,9 +10,9 @@ const FmFrag = /* glsl */`
 uniform float uTime;
 const float PI = 3.1415926;
 void main(){
-    float redV = sin(uTime * 1.0 + PI);
-    float greenV = sin(uTime * 2.0 + (PI / 2.0));
-    float blueV = sin(uTime * 0.5 + (PI / 4.0));
+    float redV = abs(sin(uTime * 1.0 + PI));
+    float greenV = abs(sin(uTime * 0.3 + (PI / 2.0)));
+    float blueV = abs(sin(uTime * 0.5 + (PI / 4.0)));
     gl_FragColor=vec4(redV, greenV, blueV, 1.);
 }
 `
