6fd6e9998ba365aa2211538d933b6d32740a9b3b
YujiTakebuchi <takeyou.swift@gmail.com>
2024-01-08 00:47:53 +0900

    法線ベクトルを使用したシェーダーのベース用意

---
+++ glsl/NormalVector.glsl.js
@@ -0,0 +1,20 @@
+const NVVert = /* glsl */`
+void main() {
+    vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+    vec4 mvPosition =  viewMatrix * worldPosition;
+    gl_Position = projectionMatrix * mvPosition;
+}
+`
+
+const NVFrag = /* glsl */`
+uniform float uTime;
+const float PI = 3.1415926;
+void main(){
+    float redV = abs(sin(uTime * 1.0 + PI));
+    float greenV = abs(sin(uTime * 0.3 + (PI / 2.0)));
+    float blueV = abs(sin(uTime * 0.5 + (PI / 4.0)));
+    gl_FragColor=vec4(redV, greenV, blueV, 1.);
+}
+`
+
+export {NVVert, NVFrag}
